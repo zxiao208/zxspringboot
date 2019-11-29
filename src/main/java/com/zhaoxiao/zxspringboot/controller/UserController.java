52d5c5516fcee9b2483e75bbe322dcffbedc4024
zhaoxiao <zxiao208@163.com>
2019-11-29 17:38:09 +0800

    1，jedis牛逼

--- src/main/java/com/zhaoxiao/zxspringboot/controller/UserController.java
+++ src/main/java/com/zhaoxiao/zxspringboot/controller/UserController.java
@@ -2,6 +2,7 @@ package com.zhaoxiao.zxspringboot.controller;
 
 import com.zhaoxiao.zxspringboot.bean.User;
 import com.zhaoxiao.zxspringboot.service.UserService;
+import com.zhaoxiao.zxspringboot.utils.JedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,10 +19,19 @@ import java.util.List;
 public class UserController {
         @Autowired
         private UserService userService;
+        @Autowired
+        private JedisUtils jedisUtils;
         @RequestMapping("login")
         public @ResponseBody String Login(){  //@ResponseBody 自动转成json格式
-                List<User> users = userService.findAllUser();
-                return users.get(0).getUserName();
+                String name=jedisUtils.get("name");
+                if(name!=null){
+                        return name;
+                }else{
+                        List<User> users = userService.findAllUser();
+                        return users.get(0).getUserName();
+                }
+
+
         }
 
 }
